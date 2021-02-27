4248bb40a3a8f0daa373d860f822df859fe5bb44
dantls <dan_tls@hotmail.com>
2021-02-27 00:34:24 -0400

    Initial commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile(){
+  return(
+    <div className={styles.profileContainer}>
+      <img 
+        src="https://avatars.githubusercontent.com/u/51554360?s=460&u=2cf2e22f5343837c6efb95259f4707bc492e5dc5&v=4"
+        alt="Danilo Gomes"
+      />
+      <div>
+        <strong>Danilo Gomes</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+
+    </div>
+  )
+}
